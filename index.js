d83e420ffb6fd5235cd5d632af6575bc542ab1f0
khrall <raymitoro@gmail.com>
2016-09-16 16:47:02 +0200

    Added CORS

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
+app.use(cors());
 
 const Pool = require('pg').Pool;
 
