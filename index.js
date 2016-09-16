240164ef71feaa77bd0ccde7c481362c6cfc404a
khrall <raymitoro@gmail.com>
2016-09-16 15:56:37 +0200

    Added some front-end from pf-dashboard

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 "use strict";
 
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
