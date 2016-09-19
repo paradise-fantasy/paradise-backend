271b21dd87cce579098cb433be9bbde2f0338b6c
khrall <raymitoro@gmail.com>
2016-09-19 14:44:02 +0200

    Added /rpi-vitals-events endpoint

--- index.js
+++ index.js
@@ -46,4 +46,17 @@ app.post('/api/bluetooth-events', (req, res) => {
     .catch(err => res.status(500).json(err))
 });
 
+app.post('/api/rpi-vitals-events', (req, res) => {
+  const load = req.body.load;
+  const temperature = req.body.temperature;
+  const memTotal = req.body.mem_total;
+  const memAvail = req.body.mem_avail;
+  const diskTotal = req.body.disk_total;
+  const diskAvail = req.body.disk_avail;
+
+  pool.query('INSERT INTO rpi_vitals(load, mem_total, mem_avail, temp, disk_total, disk_avail) VALUES ($1, $2, $3, $4, $5, $6)', [load, memTotal, memAvail, temp, diskTotal, diskAvail])
+    .then(() => res.sendStatus(200))
+    .catch(err => res.status(500).json(err))
+});
+
 app.listen(app.get('port'), () => console.log('Listening to 3000'))
